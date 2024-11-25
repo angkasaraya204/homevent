d2bc9ae8f510da22ff3d1d41ec2e1f6743d27976
Angkasa Raya <angkasaraya204@gmail.com>
2024-11-25 18:11:45 +0700

    new

--- homevent.sql
+++ homevent.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Waktu pembuatan: 25 Nov 2024 pada 09.00
+-- Waktu pembuatan: 25 Nov 2024 pada 11.10
 -- Versi server: 8.0.30
 -- Versi PHP: 8.3.12
 
@@ -24,6 +24,32 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Struktur dari tabel `acara`
+--
+
+CREATE TABLE `acara` (
+  `id` bigint UNSIGNED NOT NULL,
+  `nama` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tanggal` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `genre` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `link` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tempat` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `deskripsi` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data untuk tabel `acara`
+--
+
+INSERT INTO `acara` (`id`, `nama`, `tanggal`, `genre`, `link`, `tempat`, `deskripsi`, `created_at`, `updated_at`) VALUES
+(1, 'AKy We', '2024-11-25', 'J-Pop', 'test.com', 'Talidare Cafe, Bekasi', '18.00 - selesai', '2024-11-25 02:47:41', '2024-11-25 02:47:41'),
+(2, 'safsdfdh', '2024-11-25', 'dfhdfh', 'fhdfhdfhdd', 'dfhdfh', 'dfhdfh', '2024-11-25 03:50:17', '2024-11-25 03:50:17');
+
+-- --------------------------------------------------------
+
+--
 -- Struktur dari tabel `artikel`
 --
 
@@ -37,6 +63,13 @@ CREATE TABLE `artikel` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data untuk tabel `artikel`
+--
+
+INSERT INTO `artikel` (`id`, `judul`, `gambar`, `tanggal`, `penulis`, `konten`, `created_at`, `updated_at`) VALUES
+(1, '5 Videoklip K-Pop terbaru yang  sangat digemari', NULL, '2024-11-25', 'Kazutora', '<p>Kazutora</p>', '2024-11-25 04:09:33', '2024-11-25 04:09:33');
 
 -- --------------------------------------------------------
 
@@ -127,6 +160,17 @@ CREATE TABLE `kategori` (
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Dumping data untuk tabel `kategori`
+--
+
+INSERT INTO `kategori` (`id`, `nama`, `gambar`, `created_at`, `updated_at`) VALUES
+(1, 'Rock', NULL, '2024-11-25 02:44:57', '2024-11-25 02:44:57'),
+(2, 'K-Pop', NULL, '2024-11-25 02:44:57', '2024-11-25 02:44:57'),
+(3, 'Koplo', NULL, '2024-11-25 02:44:57', '2024-11-25 02:44:57'),
+(4, 'Cosplay', NULL, '2024-11-25 02:44:57', '2024-11-25 02:44:57'),
+(5, 'J-Pop', NULL, '2024-11-25 02:44:57', '2024-11-25 02:44:57');
+
 -- --------------------------------------------------------
 
 --
@@ -149,7 +193,8 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (3, '0001_01_01_000002_create_jobs_table', 1),
 (4, '2024_11_25_040747_create_kategori_table', 1),
 (5, '2024_11_25_055442_create_artikel_table', 1),
-(6, '2024_11_25_081603_create_tentangkami_table', 1);
+(6, '2024_11_25_081603_create_tentangkami_table', 1),
+(7, '2024_11_25_091534_create_acara_table', 1);
 
 -- --------------------------------------------------------
 
@@ -178,6 +223,14 @@ CREATE TABLE `sessions` (
   `last_activity` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Dumping data untuk tabel `sessions`
+--
+
+INSERT INTO `sessions` (`id`, `user_id`, `ip_address`, `user_agent`, `payload`, `last_activity`) VALUES
+('6MRMsajDV0ZXk7Z7hF0tjGvuNZxM72RhEN90T36P', 2, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36 Edg/131.0.0.0', 'YTo0OntzOjY6Il90b2tlbiI7czo0MDoiNEFianRRdktkV0hMOTVReEJNMTJyd096d1NIZ3M4ekR6YmViT1FCTyI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6NDE6Imh0dHA6Ly9ob21lLWV2ZW50LnRlc3QvYWRtaW4vYWNhcmEvdGFtYmFoIjt9czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319czo1MDoibG9naW5fd2ViXzU5YmEzNmFkZGMyYjJmOTQwMTU4MGYwMTRjN2Y1OGVhNGUzMDk4OWQiO2k6Mjt9', 1732532990),
+('MubJgdmSdI0bIv0OZSe5lThmdoImsChDszTfAbLF', 2, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36 Edg/131.0.0.0', 'YTo1OntzOjY6Il90b2tlbiI7czo0MDoiajNYYVI5UXdQRnRFbGFOSXJJTlN5dmlxTzdwYVJwamNESkJyRVBlbyI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MjE6Imh0dHA6Ly9wYncta2VsLTIudGVzdCI7fXM6NjoiX2ZsYXNoIjthOjI6e3M6Mzoib2xkIjthOjA6e31zOjM6Im5ldyI7YTowOnt9fXM6MzoidXJsIjthOjE6e3M6ODoiaW50ZW5kZWQiO3M6MzU6Imh0dHA6Ly9wYncta2VsLTIudGVzdC9hZG1pbi9hcnRpa2VsIjt9czo1MDoibG9naW5fd2ViXzU5YmEzNmFkZGMyYjJmOTQwMTU4MGYwMTRjN2Y1OGVhNGUzMDk4OWQiO2k6Mjt9', 1732528325);
+
 -- --------------------------------------------------------
 
 --
@@ -197,7 +250,7 @@ CREATE TABLE `tentangkami` (
 --
 
 INSERT INTO `tentangkami` (`id`, `logo`, `deskripsi`, `created_at`, `updated_at`) VALUES
-(1, NULL, 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio inventore possimus repudiandae. Assumenda officia eum optio nesciunt eligendi quae quia provident, accusantium vel, odit reprehenderit repudiandae, ratione aut impedit natus.', '2024-11-25 02:00:04', '2024-11-25 02:00:04');
+(1, NULL, 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio inventore possimus repudiandae. Assumenda officia eum optio nesciunt eligendi quae quia provident, accusantium vel, odit reprehenderit repudiandae, ratione aut impedit natus.', '2024-11-25 02:44:57', '2024-11-25 02:44:57');
 
 -- --------------------------------------------------------
 
@@ -222,12 +275,18 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `role`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'Test User', 'test@gmail.com', '2024-11-25 02:00:03', '$2y$12$6F5XOKoaMMLX8Ih5qZNfSOI3dAjCEZEKt1XCbr3c4S26QcYRj/VyG', 'tamu', 'ToP4fFIfyE', '2024-11-25 02:00:03', '2024-11-25 02:00:03'),
-(2, 'Admin', 'admin@gmail.com', '2024-11-25 02:00:04', '$2y$12$GNZT7fnTCRUs1MCEgGqlvePZdn2N.HpSozf1aQDhC9/jrTMvcjbFe', 'admin', '2ou1IXZjgW', '2024-11-25 02:00:04', '2024-11-25 02:00:04');
+(1, 'Test User', 'test@gmail.com', '2024-11-25 02:44:56', '$2y$12$wbVX3N3Mr71b5fLZT1AvOuChi8sLmXdOJLVnyMZY1Wg82llce4gWC', 'tamu', 'EVMM3kWhg1', '2024-11-25 02:44:56', '2024-11-25 02:44:56'),
+(2, 'Admin', 'admin@gmail.com', '2024-11-25 02:44:57', '$2y$12$i7rxlUsFiLZy9tVa6Uuyh.zxFuOHKqOIdWFdQFjh3LTwk5RPB/2Va', 'admin', 'cg8hI7jp4fqnYhTT31BSmZWFnQpRKCwaCKlYZUHKiqgXDftg6MF3HNnXJQjC', '2024-11-25 02:44:57', '2024-11-25 02:44:57');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indeks untuk tabel `acara`
+--
+ALTER TABLE `acara`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indeks untuk tabel `artikel`
@@ -311,10 +370,16 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT untuk tabel `acara`
+--
+ALTER TABLE `acara`
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT untuk tabel `artikel`
 --
 ALTER TABLE `artikel`
-  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT untuk tabel `failed_jobs`
@@ -332,13 +397,13 @@ ALTER TABLE `jobs`
 -- AUTO_INCREMENT untuk tabel `kategori`
 --
 ALTER TABLE `kategori`
-  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT untuk tabel `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT untuk tabel `tentangkami`
